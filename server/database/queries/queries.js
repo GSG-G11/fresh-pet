d95fccd53d9bc3441862ed73f08bc839fabccada
mo7amedehab97 <gazacityface@gmail.com>
2022-04-10 14:55:51 +0300

    create delete product query relates #5

--- server/database/queries/queries.js
+++ server/database/queries/queries.js
@@ -1,5 +1,7 @@
 const getAllProducts = 'SELECT * FROM products';
+const deleteProductQuery = 'DELETE FROM products WHERE id = ($1) RETURNING *';
 
 module.exports = {
   getAllProducts,
+  deleteProductQuery,
 };
