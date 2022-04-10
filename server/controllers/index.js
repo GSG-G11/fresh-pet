9f73c3e8d5d9cb043006ac8059249d177479946c
mo7amedehab97 <gazacityface@gmail.com>
2022-04-10 21:32:59 +0300

    add update fuction

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
-const {getAllProducts} = require('./productsController');
+const {getAllProducts, deleteProduct, updateProduct} = require('./productsController');
 const {getSingleProduct} = require('./getSingleProduct');
 
 module.exports = {
   getAllProducts,
   getSingleProduct,
+  deleteProduct,
+  updateProduct,
 };
