286496273f90f365895d78a12c9c683908376c65
mo7amedehab97 <gazacityface@gmail.com>
2022-04-10 21:33:40 +0300

    create update product query relates #28

--- server/database/queries/queries.js
+++ server/database/queries/queries.js
@@ -1,8 +1,9 @@
 const getAllProducts = 'SELECT * FROM products';
 const deleteProductQuery = 'DELETE FROM products WHERE id = ($1) RETURNING *';
-
+const updateProductQuery = 'UPDATE products SET name=$1, description=$2,  pet_category=$3, sub_category =$4 ,price =$5 , image =$6 WHERE id = $7 RETURNING name'
 
 module.exports = {
   getAllProducts,
   deleteProductQuery,
+  updateProductQuery,
 };
