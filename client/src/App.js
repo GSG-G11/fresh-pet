048c35ada85a0c3625261668330db3e55f9f3157
abedshamia <ab.shamia96@gmail.com>
2022-04-10 18:18:03 +0300

    Fixed div tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,29 @@
-
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import ProductsList from './components/Products/ProductsList';
 import ProductDetails from './components/Products/ProductDetails';
-import Header from './components/Header/Header'
-import LandingImage from './components/Header/LandingImage'
+import Header from './components/Header/Header';
+import LandingImage from './components/Header/LandingImage';
 import NotFound from './components/NotFound/NotFound';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div>
-        <Header />
-        <LandingImage />
-        <ProductsList />
-      </div>
-      <Switch>
-        <Route path='/product/:id' component={ProductDetails} /> 
-        <Route path='/notFound' component={NotFound} /> 
-        <Route path='/' component={ProductsList} exact/>
-        <Redirect to='notFound'/>
-      </Switch>
-      </div>
+        <div>
+          <Header />
+          <LandingImage />
+          <ProductsList />
+        </div>
+        <Switch>
+          <Route path="/product/:id" component={ProductDetails} />
+          <Route path="/notFound" component={NotFound} />
+          <Route path="/" component={ProductsList} exact />
+          <Redirect to="notFound" />
+        </Switch>
       </BrowserRouter>
     );
   }
 }
 
 export default App;
-
