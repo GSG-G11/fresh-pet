c7e4c45f179bdd1b97bf807d85d3f967b0e94590
mo7amedehab97 <gazacityface@gmail.com>
2022-04-10 15:19:53 +0300

    fix delete status code relaetes #5

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -1,22 +1,22 @@
-const connection = require('../database/config/connection');
-const queries = require('../database/queries/queries');
+const connection = require('../database/config/connection')
+const queries = require('../database/queries/queries')
 
 const getAllProducts = async (req, res) => {
-  const products = await connection.query(queries.getAllProducts);
+  const products = await connection.query(queries.getAllProducts)
 
   res.status(200).json({
     status: 'success',
     products: products.rows,
-  });
-};
+  })
+}
 
 const deleteProduct = async (req, res) => {
-  const { id } = req.params;
-  await connection.query(queries.deleteProductQuery, [id]);
-  res.status(201).json({ msg: 'deleted product succes' });
-};
+  const { id } = req.params
+  await connection.query(queries.deleteProductQuery, [id])
+  res.status(204)
+}
 
 module.exports = {
   getAllProducts,
   deleteProduct,
-};
+}
