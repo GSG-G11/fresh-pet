2043b500e50087d6916565a807a65a268d3b417c
mo7amedehab97 <gazacityface@gmail.com>
2022-04-10 21:34:52 +0300

    create update product route

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,9 +3,12 @@ const {
   getAllProducts,
   getSingleProduct,
   deleteProduct,
+  updateProduct,
 } = require('../controllers');
 router.get('/', getAllProducts);
 router.get('/product/:id', getSingleProduct);
 router.delete('/product/:id', deleteProduct);
+router.patch('/product/:id', updateProduct);
+
 
 module.exports = router;
