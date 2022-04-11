63ede26a75d543b448e7d3e3f643c65ae5c8d37b
abedshamia <ab.shamia96@gmail.com>
2022-04-11 15:47:57 +0300

    Change Button style and add back btn to product details

--- client/src/components/Buttons/AddToCard.jsx
+++ client/src/components/Buttons/AddToCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
-const addToCartStorage = (props) => {
+const addToCartStorage = props => {
   const products = JSON.parse(localStorage.getItem('products'));
   products.push(props.product);
   localStorage.setItem('products', JSON.stringify(products));
 };
-const AddToCard = (props) => {
+const AddToCard = props => {
   const {product} = props;
   return (
-    <button className='add-to-cart' onClick={() => addToCartStorage(product)}>
-      Add to Cart
+    <button className="add-to-cart" onClick={() => addToCartStorage(product)}>
+      <FontAwesomeIcon icon={faCartShopping} />
     </button>
-  )
-}
+  );
+};
 
 export default AddToCard;
