9bb6a99cc06aa5b71ced5c0df1c7444111be7a72
mo7amedehab97 <gazacityface@gmail.com>
2022-04-11 13:53:42 +0300

    update selected input value relates#36

--- client/src/components/Forms/UpdateProduct.jsx
+++ client/src/components/Forms/UpdateProduct.jsx
@@ -13,26 +13,28 @@ const UpdateProduct = ({
       <h1 className={styles.title__modal__product}> Update Product </h1>
       <form className={styles.form__group__product}>
         <div className={styles.form__input__product}>
-          <label htmlFor='name'>Name</label>
+          <label htmlFor="name">Name</label>
           <input
-            type='text'
-            name='name'
-            id='name'
+            type="text"
+            name="name"
+            id="name"
             value={name}
             onChange={handleChange}
-            placeholder='Enter Your Name of product...'
+            placeholder="Enter Your Name of product..."
           />
         </div>
         <div className={styles.form__input__product}>
-          <label htmlFor='petCategory'>Pet Category</label>
+          <label htmlFor="petCategory">Pet Category</label>
 
-          <select id='petCategory' name='petCategory' onChange={handleChange}>
+          <select
+            id="petCategory"
+            value={petCategory}
+            name="petCategory"
+            onChange={handleChange}
+          >
             {petCategories.map((category) => {
               return (
-                <option
-                  key={category}
-                  selected={petCategory === category}
-                  value={category}>
+                <option key={category} value={category}>
                   {category}
                 </option>
               );
@@ -40,15 +42,19 @@ const UpdateProduct = ({
           </select>
         </div>
         <div className={styles.form__input__product}>
-          <label htmlFor='subCategory'>Sub Category</label>
+          <label htmlFor="subCategory">Sub Category</label>
 
-          <select name='subCategory' onChange={handleChange}>
+          <select
+            name="subCategory"
+            value={subCategory}
+            onChange={handleChange}
+          >
             {categories.map((category) => {
               return (
                 <option
                   key={category}
-                  selected={subCategory === category}
-                  value={category}>
+                  value={category}
+                >
                   {category}
                 </option>
               );
@@ -56,48 +62,50 @@ const UpdateProduct = ({
           </select>
         </div>
         <div className={styles.form__input__product}>
-          <label htmlFor='price'>Price</label>
+          <label htmlFor="price">Price</label>
           <input
-            type='number'
-            name='price'
-            id='price'
+            type="number"
+            name="price"
+            id="price"
             value={price}
             onChange={handleChange}
-            placeholder='Enter Your price of product...'
+            placeholder="Enter Your price of product..."
           />
         </div>
         <div className={styles.form__input__product}>
-          <label htmlFor='image'>Image</label>
+          <label htmlFor="image">Image</label>
           <input
-            type='url'
-            name='image'
-            id='image'
+            type="url"
+            name="image"
+            id="image"
             value={image}
             onChange={handleChange}
-            placeholder='Enter Your image of product...'
+            placeholder="Enter Your image of product..."
           />
         </div>
         <div className={styles.form__input__product}>
-          <label htmlFor='description'>Description</label>
+          <label htmlFor="description">Description</label>
           <textarea
-            id='description'
-            name='description'
+            id="description"
+            name="description"
             value={description}
             onChange={handleChange}
-            placeholder='Enter Your description...'>
+            placeholder="Enter Your description..."
+          >
             {description}
           </textarea>
         </div>
         {hasErrorValidation && (
           <div className={styles.form__input__product}>
-            <p className='error'>Inputs Should not be empty!</p>
+            <p className="error">Inputs Should not be empty!</p>
           </div>
         )}
         <div className={styles.form__input__product}>
           <button
             className={styles.btn__create__product}
-            type='button'
-            onClick={handleUpdateProduct}>
+            type="button"
+            onClick={handleUpdateProduct}
+          >
             Submit
           </button>
         </div>
