24d5d048b2ef5c455053b7a00107b59184e25391
abedshamia <ab.shamia96@gmail.com>
2022-04-12 15:01:55 +0300

    Replaced Background image and added bone image

--- client/src/components/Header/LandingImage.jsx
+++ client/src/components/Header/LandingImage.jsx
@@ -4,8 +4,9 @@ import './Header.css';
 
 const LandingImage = () => {
   return (
-    <div className='background-image'>
-      <img src='../img/background.jpg' alt='dog img' className='dog-img' />
+    <div className="background-image">
+      <img src="../img/background.jpg" alt="dog img" className="dog-img" />
+      <img src="../img/bone.png" alt="bone" className="bone-img" />
     </div>
   );
 };
