ac60ca405ec89f1c655303df601ea24a36c9cbc7
Jehad91 <j.abushaqra1@gmail.com>
2022-04-12 11:58:37 +0300

    upade Relates #50

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
-import { Product } from '../Products';
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+//import { Delete } from '../Buttons';
+import Counter from './Counter';
+import './cartStyle.css';
 
-const Cart = () => {
-  const products = JSON.parse(localStorage.getItem('products'));
-  const cartProducts = products.map((product) => (
-    <Product
-    key={product.id}
-    product={product}
-    />
-  ))
-
-  let sum = 0;
-  products.forEach((element) => {
-    sum += +element.price
-  });
-  return(
-    <>
-    <div className='container'>
-        <h3>Total Price: {sum.toFixed(2)}$</h3>
-        <section className='products-section'>
-          {products.length === 0 && (
-            <h1>No Products Found</h1>
-          )}
-          {cartProducts}
-        </section>
-    </div>
-    </>
-  )
+class Cart extends Component{
+  state={
+    products: JSON.parse(localStorage.getItem('products'))
+  }
+  render() {
+    const {products} = this.state;
+    let sum = 0;
+    products.forEach((element) => {
+      sum += +element.price;
+    });
+    return(
+      <>
+      <div className='container'>
+          <h3>Total Price: {sum.toFixed(2)}$</h3>
+          <section className='products-cart-section'>
+            <ul className='cart-products-list'>
+            { products.length ? products.map((product) => (
+              <Counter product={product}/>
+            )) : <h1>No Products Found</h1>}
+  
+            </ul>
+            <div className='total-cost-holder'>
+              <h3 className='total-cost'>Total Cost: <span>${sum.toFixed(2)}</span></h3>
+            </div>
+          </section>
+      </div>
+      </>
+    )
+    
+  }
 }
 export default Cart;
