46c74360dc63d17e3483dc87150b3ce4429c58c5
abedshamia <ab.shamia96@gmail.com>
2022-04-12 15:33:25 +0300

    Add content to hero section

--- client/src/components/Header/LandingImage.jsx
+++ client/src/components/Header/LandingImage.jsx
@@ -5,6 +5,9 @@ import './Header.css';
 const LandingImage = () => {
   return (
     <div className="background-image">
+      <h1 className="slang">
+        Everything your pet needs <br /> in one place
+      </h1>
       <img src="../img/background.jpg" alt="dog img" className="dog-img" />
       <img
         src="../img/bone.png"
