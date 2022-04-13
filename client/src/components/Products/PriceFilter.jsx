623e8c40de3ccd691ed73e48a3a573e45920e10d
Ahmed Qeshta <ahmedqeshta1999@gmail.com>
2022-04-14 02:09:58 +0300

    Fix Bug in Filter by price and fix class name and remove not needed input

--- client/src/components/Products/PriceFilter.jsx
+++ client/src/components/Products/PriceFilter.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import './petFilterStyle.css';
-const PriceFilter = ({priceRange, handlePriceRange}) => {
-
+const PriceFilter = ({ priceRange, handlePriceRange, products }) => {
+  const max = Math.max.apply(
+    Math,
+    products.map(({ price }) => price),
+  );
   return (
     <>
-      <div class="wrapper">
-      <fieldset class="filter-price">
-        <div class="price-field">
-          <input type="range"  min="0" max="200" onChange={(e) => handlePriceRange(+e.target.value, priceRange[1])} value={priceRange[0]} id="lower"/>
-          <input type="range" min="0" max="200" onChange={(e) => handlePriceRange(priceRange[0], +e.target.value)} value={priceRange[1]} id="upper"/>
-        </div>
-        <div class="price-wrap">
-          <div class="price-wrap-1">
-            <input id="one" value={priceRange[0]}/>
-            <span>$</span>
+      <div className='wrapper'>
+        <fieldset className='filter-price'>
+          <div className='price-field'>
+            <input
+              type='range'
+              min='0'
+              max={max}
+              onChange={(e) => handlePriceRange(+e.target.value, priceRange[1])}
+              value={priceRange[0]}
+              id='lower'
+            />
+            <input
+              type='range'
+              min='0'
+              max={max}
+              onChange={(e) => handlePriceRange(priceRange[0], +e.target.value)}
+              value={priceRange[1]}
+              id='upper'
+            />
           </div>
-          <div class="price-wrap-2">
-            <input id="two" value={priceRange[1]}/>
-            <span>$</span>
+          <div className='price-wrap'>
+            <div className='price-wrap-1'>
+              <span>$ {priceRange[0]}</span>
+            </div>
+            <div className='price-wrap-2'>
+              <span>$ {priceRange[1]}</span>
+            </div>
           </div>
-        </div>
-      </fieldset> 
-    </div>
+        </fieldset>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default PriceFilter;
-
